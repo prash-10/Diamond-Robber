9ed12e9eeac3ebb6f752a9c8906ddae190cc72c3
prash-10 <prashantsingh1896@gmail.com>
2016-11-08 01:50:38 +0530

    Add files via upload

---
+++ menu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour {
+	public GameObject g;
+	public GameObject g1;
+
+	//public static GameObject StartGame;
+
+	public void LoadScene(int level)
+	{
+		print ("Entered");
+		if(g.tag== "Start Game"&&level == 2)
+			{
+			SceneManager.LoadScene ("Level 1");
+		}
+
+		else if(g1.tag=="Instructions"&&level == 1)
+	{
+			SceneManager.LoadScene ("Instructions");
+	}
+}
+}
